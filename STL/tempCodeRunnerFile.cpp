2e04dc4282b18adb06aa5b776f72895874fd8bcd
Priya <priyabansal.cse@gmail.com>
2021-05-15 23:04:24 +0530

    first commit

---
+++ STL/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+map<string,int> mp;
+    // mp["p"]=22;
+    // mp["a"]=8;
+    // mp["v"]=92;
+    // map<string,int>:: iterator it;
+    // cout<<"Key"<<" "<<"Value";
+    // for(it=mp.begin();it!=mp.end();it++){
+    //     cout<<it->first<<" "<<it->second<<endl;
+    // }
