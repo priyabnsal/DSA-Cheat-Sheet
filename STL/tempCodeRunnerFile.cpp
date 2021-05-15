98a1665629fce080465d4f487215966335f30fe2
Priya <priyabansal.cse@gmail.com>
2021-05-15 23:07:07 +0530

    done

--- STL/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-map<string,int> mp;
-    // mp["p"]=22;
-    // mp["a"]=8;
-    // mp["v"]=92;
-    // map<string,int>:: iterator it;
-    // cout<<"Key"<<" "<<"Value";
-    // for(it=mp.begin();it!=mp.end();it++){
-    //     cout<<it->first<<" "<<it->second<<endl;
-    // }
