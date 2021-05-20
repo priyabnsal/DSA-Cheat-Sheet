e9e64846230ff69809ffb7999a6939f348f8e9be
Priya <priyabansal.cse@gmail.com>
2021-05-20 17:32:17 +0530

    3

---
+++ Recursion/3_SumofArray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int findSum(int A[], int N)
+{
+	if (N <=0)
+		return 0;
+	return (findSum(A, N - 1) + A[N - 1]);
+}
+
+int main()
+{
+	int A[] = { 1, 2, 3};
+	int N = sizeof(A) / sizeof(A[0]);
+    cout<<findSum(A, N);
+	return 0;
+}
