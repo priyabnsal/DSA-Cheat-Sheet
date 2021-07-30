66776ef31ecca6dc52319b19fb4f48b2b275bb40
Priya Bansal <priyabansal.cse@gmail.com>
2021-07-30 23:20:02 +0530

    14

---
+++ Binary Search/14_findPeakelement.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num) 
+    {
+        int low = 0;
+        int high = num.size()-1;
+        
+        while(low < high)
+        {
+            int mid1 = (low+high)/2;
+            int mid2 = mid1+1;
+            if(num[mid1] < num[mid2])
+                low = mid2;
+            else
+                high = mid1;
+        }
+        return low;
+    }
+};
+
+////////////////////////////////////////////////////////
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int start=0;
+        int  end= n-1;
+        while (start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if (mid>0 and mid<n-1) // this will check btw the 1st and last// except 1st and last it will chech throughout
+            {
+                if (nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+                    return mid;
+                else if (nums[mid-1]>nums[mid] && nums[mid+1])
+                    end=mid-1;
+                else 
+                    start=mid+1;
+            }
+            else if (mid==0)
+            {
+                if (nums[0]>nums[1])
+                    return 0;
+                else
+                    return 1;
+            }
+            else if(mid==n-1)
+            {
+                if (nums[n-1]>nums[n-2])
+                    return n-1;
+                else
+                    return n-2;
+            }
+        }
+        return 0;
+        
+    }
+};
